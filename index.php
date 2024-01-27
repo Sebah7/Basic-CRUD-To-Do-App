83ba393c88119781aca304215c4ce3166f214e22
Sebah Abubeker <sebahismail21@gmail.com>
2024-01-27 15:45:25 +0000

    index done

--- index.php
+++ index.php
@@ -4,33 +4,11 @@ include 'setup.php';
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ToDo App</title>
-</head>
-<body>
-    <h2>Add a task</h2>
-    <form action="create.php" method="post">
-        <label for="title">Title:</label>
-        <input type="text" name="title" required>
-        
-        <label for="description">Description:</label>
-        <textarea name="description" required></textarea>
-        
-        <label for="done">Done:</label>
-        <input type="checkbox" name="done" value="1">
+<?=header_temp('Home')?>
 
-        <button type="submit">Add Task</button>
-    </form>
+<div class="content">
+	<h2>Todo_App</h2>
+	<p>Kepp track of your tasks with the Todo_App!</p>
+</div>
 
-    <h2>Task List</h2>
-    <?php
-include 'read.php'; // Include the script to display tasks
-?>
-
-</body>
-</html>
+<?=footer_temp()?>
