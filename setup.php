497d6e7a975cf3eea9e655737d63e275cb663ba3
Sebah Abubeker <sebahismail21@gmail.com>
2024-01-28 14:40:36 +0000

    added icon

--- setup.php
+++ setup.php
@@ -29,6 +29,9 @@ echo <<<EOT
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" type="image/png" href="media/icon.png">
+    <meta name="author" content="Sebah Abubeker">
+    <meta name="description" content="Track your tasks and be on top!">
 		<title>$title</title>
 		<link href="style.css" rel="stylesheet" type="text/css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
